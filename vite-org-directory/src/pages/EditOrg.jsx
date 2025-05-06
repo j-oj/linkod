e74509f4d7778696700cb24a2620aa34b0f61636
Jan <139526981+j-oj@users.noreply.github.com>
2025-05-07 01:13:53 +0800

    try: change navbar  to lowercase

--- vite-org-directory/src/pages/EditOrg.jsx
+++ vite-org-directory/src/pages/EditOrg.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { supabase } from "../supabaseClient";
-import Navbar from "../components/Navbar";
-import Loading from "../components/Loading";
+import Navbar from "../components/navbar";
+import Loading from "../components/loading";
 import Select from "react-select";
 import {
   FaFacebook,
